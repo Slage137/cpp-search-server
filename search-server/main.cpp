d659b00991851771ad39ab255d5590224323d1f8
Maksim1473 <slage137@yandex.ru>
2022-08-08 21:41:47 +0300

    My first commit

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,21 @@
-// Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
-// Напишите ответ здесь:
+#include <iostream>
+#include <string>
 
-// Закомитьте изменения и отправьте их в свой репозиторий.
+using namespace std;
+
+// Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
+int main() {
+	int cnt = 0;
+	for (int i = 1; i <= 1000; i++) {
+		string number;
+		number = to_string(i);
+		for (auto j : number) {
+			if (j == '3') {
+				cnt++;
+				break;
+			}
+		}
+	}
+	cout << cnt << endl;
+
+}
